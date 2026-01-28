757954dc1e37f09a8fecc46bedd956e2947e190b
Abinash Mohapatra <mabinash016@gmail.com>
2026-01-28 20:24:58 +0530

    integrated pwa for download option in mobiles and laptops

--- sw.js
+++ sw.js
@@ -1,31 +1,10 @@
-const CACHE = "react-notes-cache-v2";
-
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./styles.css"
-      ])
-      .catch(err => {
-        console.log("Some files not cached:", err);
-      })
-    )
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", e => {
-
-  // Don’t try to cache external CDN files
-  if(e.request.url.startsWith("http") &&
-     !e.request.url.includes(location.origin)){
-    return;
-  }
-
+  // Network first (no aggressive caching)
   e.respondWith(
-    caches.match(e.request).then(res =>
-      res || fetch(e.request)
-    )
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
